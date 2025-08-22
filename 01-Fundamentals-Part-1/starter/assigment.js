2de0116031e28e1881cab7c1a49d10901f87ab04
Kevin Vanegas <kevin@kevinvanegas.com>
2025-08-22 19:10:54 -0400

    starter editing

---
+++ 01-Fundamentals-Part-1/starter/assigment.js
@@ -0,0 +1,5 @@
+let country = 'El Salvador';
+let continent = 'Central America';
+let population = '6.36 million';
+
+console.log(`${country} is located in the continent of ${continent} with a population of ${population}.`)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log('Jonas');
+console.log(23)
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
